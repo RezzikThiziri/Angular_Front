cea1cce06ca0b5c56f78dba38a37a80267e58e90
RezzikThiziri <thizirirezzik1999@gmail.com>
2023-11-30 23:03:30 +0100

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,30 @@ import { TicketService } from '../services/ticket/ticket.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 
+import { HttpClientModule } from '@angular/common/http'; // Importer le module HttpClientModule
+import { StudentListComponent } from './students/student-list/student-list.component';
+import { StudentFormComponent } from './students/student-form/student-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentComponent } from './students/student/student.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     TicketComponent,
     TicketFormComponent,
     TicketListComponent,
-    HeaderComponent // All the components needs to be declared
+    HeaderComponent, // All the components needs to be declared
+    StudentListComponent, //ajout composant StudentListComponent
+    StudentFormComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule // Import all dependencies
+    AppRoutingModule,
+    ReactiveFormsModule, // Import all dependencies
+    HttpClientModule, // Ajouter HttpClientModule dans la liste des imports
+   
+     
   ],
   providers: [TicketService], // All the services need to be provided
   bootstrap: [AppComponent]
